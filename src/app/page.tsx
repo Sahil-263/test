ab21bcaa87d164703727e5efe818f9930ccb49fe
Sahil-263 <sahilarora2063@gmail.com>
2024-08-04 19:00:03 +0530

    blank

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AuroraBackgroundDemo from "./landingpage";
-import SidebarDemo from "./home"; // Make sure the import path is correct
+ // Make sure the import path is correct
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<AuroraBackgroundDemo />} />
-        <Route path="/home" element={<SidebarDemo />} />
       </Routes>
     </Router>
   );
